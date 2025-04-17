73570c1daa77a53abe53d15c13dd2f6c289759ae
Celsea Mega <celseamega21@gmail.com>
2025-04-17 16:32:13 +0700

    modified url

--- scrapping_ecommerce/settings.py
+++ scrapping_ecommerce/settings.py
@@ -26,11 +26,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['ecommerce-price-alert.up.railway.app']
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ecommerce-price-alert.up.railway.app']
-
+ALLOWED_HOSTS = ['ecommerce-price-alert.up.railway.app']
 
 # Application definition
 
