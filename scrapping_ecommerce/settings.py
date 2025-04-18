2aa2e5785325c0aa15e66d653ecb5a20d9b62935
Celsea Mega <celseamega21@gmail.com>
2025-04-18 17:50:33 +0700

    update

--- scrapping_ecommerce/settings.py
+++ scrapping_ecommerce/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,14 +80,7 @@ WSGI_APPLICATION = 'scrapping_ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'railway',
-        'USER': 'root',
-        'PASSWORD': os.getenv("DB_PASS_RAILWAY"),
-        'HOST': 'mysql.railway.internal', 
-        'PORT': 3306
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
