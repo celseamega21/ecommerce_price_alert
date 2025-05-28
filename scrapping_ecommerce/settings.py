c9b4151d6c1f1921db4ad4a3525a542dd0ddf771
Celsea Mega <celseamega21@gmail.com>
2025-05-28 20:55:28 +0700

    Update settings.py

--- scrapping_ecommerce/settings.py
+++ scrapping_ecommerce/settings.py
@@ -27,10 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.100']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '192.168.1.100']
 
 # Application definition
 
